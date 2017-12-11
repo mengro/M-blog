94d9c9642f9acfe3d55d82ead28f1681ae4a1923
mengro <550494402@qq.com>
2017-12-11 15:51:26 +0800

    add home.vue
    add home.vue

---
+++ src/pages/home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="home">
+        <div class="log">
+            Black Blog
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'Home',
+        components: {
+
+        }
+    }
+</script>
+<style scoped>
+    .home{
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+    .log{
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        height: 100%;
+        text-align: center;
+        width: 400px;
+        height: 80px;
+        line-height: 50px;
+        margin-left: -200px;
+        margin-top: -40px;
+        font-size: 80px;
+        color: #fff;
+    }
+</style>
